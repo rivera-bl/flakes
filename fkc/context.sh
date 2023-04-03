ffbb2b72738031377008fc36089af4eb8daf9002
rivera-bl <rivera.pablo1090@gmail.com>
2023-04-03 17:30:40 -0400

    fkc: todo

--- fkc/context.sh
+++ fkc/context.sh
@@ -3,6 +3,7 @@
 # TODO create table with account, context, cluster
 # TODO export AWS_PROFILE env
 # TODO should set per session with tmux env?
+# TODO sent to every pane
 
 find ~/.kube/ -maxdepth 1 -not -type d -printf "%f\n" \
-  | fzf --bind 'enter:execute(tmux send-keys "tenv KUBECONFIG ~/.kube/{1} && clear" Enter)+abort'
+  | fzf-tmux -p --border --bind 'enter:execute(tmux setenv KUBECONFIG ~/.kube/{1} && clear)+abort' | true
