36e8f86f4806abb0e426b0cb28bf98c13dfb04f5
rivera-bl <rivera.pablo1090@gmail.com>
2023-04-03 21:04:18 -0400

    kfc: readability

--- kfc/context.sh
+++ kfc/context.sh
@@ -8,5 +8,7 @@ context=$(find ~/.kube/ -maxdepth 1 -not -type d -printf "%f\n" | fzf-tmux -p --
 
 if [ ! -z "${context}" ]; then
   tmux setenv KUBECONFIG ~/.kube/${context}
-  tmux list-panes -s -F '#{pane_id} #{pane_current_command}' | grep 'zsh' | cut -d' ' -f1 | xargs -I {} tmux send-keys -t {} "export KUBECONFIG=~/.kube/${context} && clear" Enter
+  tmux list-panes -s -F '#{pane_id} #{pane_current_command}' \
+    | grep 'zsh' | cut -d' ' -f1 | xargs -I {} \
+    tmux send-keys -t {} "export KUBECONFIG=~/.kube/${context} && clear" Enter
 fi
