a7f908393dd5b503c8ef2521ffce87eba137c4b2
rivera-bl <rivera.pablo1090@gmail.com>
2023-04-03 21:02:58 -0400

    fkc: change name to kfc

--- fkc/context.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# TODO create table with account, context, cluster
-# TODO export AWS_PROFILE env
-# TODO should set per session with tmux env?
-# TODO sent to every pane
-
-find ~/.kube/ -maxdepth 1 -not -type d -printf "%f\n" \
-  | fzf-tmux -p --border --bind 'enter:execute(tmux setenv KUBECONFIG ~/.kube/{1} && clear)+abort' | true
