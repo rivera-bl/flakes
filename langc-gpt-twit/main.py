c3b80ab2fd03ba8fa4f1cf3fb6645cfa741deb5d
rivera-bl <rivera.pablo1090@gmail.com>
2023-05-13 11:36:25 -0400

    lang: fuzzy match exclusions

--- langc-gpt-twit/main.py
+++ langc-gpt-twit/main.py
@@ -1,3 +1,4 @@
+import fnmatch
 import os
 from langchain.vectorstores import FAISS
 from langchain.embeddings.openai import OpenAIEmbeddings
@@ -11,12 +12,12 @@ from langchain.chains import RetrievalQA
 
 openai_api_key = os.environ["OPENAI_API_KEY"]
 root_dir = '/home/wim/code/personal/flakes'
-# TODO accept glob patterns
 exclude = [
-    ".git",
-    "langc-gpt-twit/.venv-langc-gpt-twit"
+    "**/.git*",
+    "**/.venv*",
+    "**/.direnv"
 ]
-query = "Explain what is the goal of this code"
+query = "Explain what is the goal of the code gla"
 
 llm = ChatOpenAI(model='gpt-3.5-turbo', openai_api_key=openai_api_key)
 
@@ -35,8 +36,7 @@ for dirpath, dirnames, filenames in os.walk(root_dir):
     # Go through each file
     for file in filenames:
         dir = os.path.join(dirpath, file)
-        # Exclude paths from the exclude list
-        if any(excl in dir for excl in exclude):
+        if any(fnmatch.fnmatch(dir, pattern) for pattern in exclude):
             continue
         try:
             # Load up the file as a doc and split
