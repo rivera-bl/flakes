604e341174dba1d75e622ea08a749523d3ef5999
rivera-bl <rivera.pablo1090@gmail.com>
2023-05-27 23:09:35 -0400

    templates: add files for nix python projects

---
+++ _docs/templates/nix/python/default.nix
@@ -0,0 +1,17 @@
+with import <nixpkgs> { };
+let
+  filePath = builtins.toString ./. + "/requirements.txt";
+  pipModules = builtins.replaceStrings [ "\n" ] [ " " ] (builtins.readFile filePath);
+in
+mkShell {
+  name = "";
+  buildInputs = with python3Packages; [ venvShellHook ];
+  venvDir = ".venv-";
+  postShellHook = ''
+    echo "Installing Pip Modules defined in ${filePath}"
+    pip install -q --disable-pip-version-check ${pipModules}
+    echo -e "${pipModules}"
+    unset KUBECONFIG
+    printf "\033c"
+  '';
+}
