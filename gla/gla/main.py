fc215d2640ff89e8f53f6fe50f6397327c082b52
rivera-bl <rivera.pablo1090@gmail.com>
2023-05-31 22:41:23 -0400

    gla: refactor

--- gla/gla/main.py
+++ gla/gla/main.py
@@ -1,76 +1,64 @@
 import os
-import re
-import json
 import subprocess
 import sys
 import gitlab
+import csv
+import argparse
 
 GITLAB_SERVER = os.environ.get('GL_SERVER', 'https://gitlab.com')
 GITLAB_TOKEN = os.environ.get('GL_TOKEN')
-cache_path  = '/tmp/gla/projects.csv'
+cache_path = '/tmp/gla/projects.csv'
 open_bin = 'wsl-open'
 
-# TODO: Refactor to json, to use headers for setting a date of last imported/updated
-# TODO: Jump to repo when finished downloading
-# TODO: Add options: -l -> for import/update on command, -e -> for execute (default)
-# TODO: don't show in output the server url, just the repository path
-# TODO: --preview to describe repository
-# TODO: --preview to get vars
-# TODO: command to watch pipeline
+def get_projects():
+    if not GITLAB_TOKEN:
+        print("Please set the GL_TOKEN env variable.")
+        sys.exit(1)
+    gl = gitlab.Gitlab(GITLAB_SERVER, GITLAB_TOKEN)
+    gl.auth()
+
+    projects = gl.projects.list(per_page=7, all=False)
+    project_list = []
+
+    # Add the header to the project_list
+    header = ['Id', 'Repository']
+    project_list.append(header)
+
+    for project in projects:
+        project_info = [str(project.id), project.ssh_url_to_repo]
+        project_list.append(project_info)
+    return project_list
+
+def write_csv(project_list):
+    os.makedirs(os.path.dirname(cache_path), exist_ok=True)
+    with open(cache_path, 'w', newline='') as f:
+        writer = csv.writer(f)
+        writer.writerows(project_list)
+
 def main():
-    if not os.path.isfile(cache_path):
-        if not GITLAB_TOKEN:
-            print("Please set the GL_TOKEN env variable.")
-            sys.exit(1)
-        gl = gitlab.Gitlab(GITLAB_SERVER, GITLAB_TOKEN)
-        gl.auth()
+    parser = argparse.ArgumentParser(description='Load GitLab projects and execute actions with Fzf.')
+    parser.add_argument('--load', action='store_true', help=f'Load projects into {cache_path}')
+    args = parser.parse_args()
 
-        # projects = gl.projects.list(page=1, per_page=5)
-        projects = gl.projects.list(all=True)
-        csv = "id,ssh_url_to_repo\n"
-        for project in projects:
-            # only exclude group if argument is passed
-            if len(sys.argv) == 2:
-                # if string before first '/' of project.path_with_namespace is not argument, then continue to the next iteration
-                if project.path_with_namespace.split('/')[0] != sys.argv[1]: continue
-            # print(json.dumps(project.attributes, indent=4, sort_keys=True))
-            values = [
-                str(project.id),
-                project.ssh_url_to_repo
-            ]
-            csv += ','.join(values) + "\n"
+    command = f"""cat {cache_path} \
+                 | sed 's/,[^,]*\/\/[^\/]*\//,/; s/\.[^.]*$//'  \
+                 | column -t -s, \
+                 | fzf --header-lines=1 \
+                    --header "| enter:clone | ctrl-o:open |" \
+                    --prompt="projects>"\
+                    --height=80% \
+                    --preview-window right,hidden,60% \
+                    --preview "aws ec2 describe-instances --instance-ids {{1}} | bat -l json --color always" \
+                        --bind "ctrl-h:execute-silent(tmux select-pane -L)" \
+                        --bind 'ctrl-space:toggle-preview'
+                 """
 
-        os.makedirs(os.path.dirname(cache_path), exist_ok=True)
-        with open(cache_path, 'w') as f:
-            f.write(csv)
+    if args.load or not os.path.isfile(cache_path):
+        projects = get_projects()
+        write_csv(projects)
 
-    # use shell column command to convert csv into columns and pipe the result to fzf shell command
-    csv     = subprocess.Popen(['cat', cache_path], stdout=subprocess.PIPE)
-    columns = subprocess.Popen(['column', '-t', '-s,'], stdin=csv.stdout, stdout=subprocess.PIPE)
-    fzf     = subprocess.Popen(['fzf',
-                                '--prompt=projects>',
-                                "--header=\ enter: clone \ ctrl-o: open \\",
-                                '--min-height=20',
-                                '--header-lines=1',
-                                '--multi',
-                                '--bind=enter:execute-multi(echo -n clone,{2})+abort',
-                                '--bind=ctrl-o:execute(echo -n open,{2})+abort'
-                                ], stdin=columns.stdout, stdout=subprocess.PIPE)
-    selraw = fzf.stdout.read()
-    if not selraw: exit()
-    sel = selraw.decode('utf-8').split()
+    subprocess.run(command, shell=True)
 
-    if sel[0].startswith('clone'):
-        for i in sel:
-            url = i.replace('clone,', '')
-            dir = url.replace('.git', '')
-            for i in range(4):
-                dir = dir[dir.find('/')+1:]
-            # git clone sel into home user directory + code/gitlab + dir
-            subprocess.run(['git', 'clone', url, os.path.expanduser('~/code/gitlab/') + dir])
-    elif sel[0].startswith('open'):
-        for i in sel:
-            url = i.replace('open,', '').replace('ssh://git@', 'https://')
-            # use regex to remove the port number
-            url = re.sub(r':\d+/', '/', url)
-            subprocess.run([open_bin, url])
+
+if __name__ == "__main__":
+    main()
